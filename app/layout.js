f6575b913756cd9b46f60727ab29b51786742824
Daniel <daniel101shi@gmail.com>
2025-10-05 00:37:02 -0700

    added overflow-hidden

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-[var(--background)] text-[var(--foreground)] antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-[var(--background)] text-[var(--foreground)] antialiased overflow-hidden`}
       >
         <main>{children}</main>
       </body>
